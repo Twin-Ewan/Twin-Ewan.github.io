f518753935c5cc7bef58addb8fe979bbff796c6b
Twin-Ewan <ewan239clark@gmail.com>
2026-02-19 22:32:32 +0000

    Created Generic MediaElement for AltText

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-      site: 'https://Twin-Ewan.github.io',
+  site: 'https://Twin-Ewan.github.io',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
