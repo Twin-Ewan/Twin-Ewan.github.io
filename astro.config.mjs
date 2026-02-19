0ca462de4bd8f012d96db2e7ae576f27a560dc88
Twin-Ewan <ewan239clark@gmail.com>
2026-02-19 23:33:36 +0000

    desprately trying to find something to fix this

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Twin-Ewan.github.io',
+  site: 'https://twin-ewan.github.io',
 
   adapter: node({
     mode: 'standalone',
