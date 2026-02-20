6884cd33086be0cd84cc24f4f98f7a4851111f02
Twin-Ewan <ewan239clark@gmail.com>
2026-02-20 00:42:17 +0000

    nuked node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://twin-ewan.github.io',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  site: 'https://twin-ewan.github.io'
 });
